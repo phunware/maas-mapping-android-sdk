c7cb66b61ad42d085d697215c8683cc863b5e428
npike <npike@phunware.com>
2019-10-28 11:48:57 -0700

    SWMAAS-1348 Update kotlin sample colors to match design

---
+++ Samples/kotlin/src/main/java/com/phunware/kotlin/sample/utils/extensions/Context.kt
@@ -0,0 +1,19 @@
+package com.phunware.kotlin.sample.utils.extensions
+
+import android.content.Context
+import android.util.TypedValue
+import com.phunware.kotlin.sample.R
+
+/**
+ * Returns the accent color as a ColorInt for the theme on the current context.
+ */
+fun Context.accentColor(): Int {
+    val typedValue = TypedValue()
+
+    val a = obtainStyledAttributes(typedValue.data, intArrayOf(R.attr.colorAccent))
+    val color = a.getColor(0, 0)
+
+    a.recycle()
+
+    return color
+}
